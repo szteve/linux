822cdf1216d9dde90c899653e06e752f17d724d3
szteve <steveoneill1994@gmail.com>
2016-04-13 12:30:09 +0100

    files have been sent to github

---
+++ forkLab06-1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+   pid_t pid;
+   int tmp=10;
+
+   printf ("the main program process ID is: %d\n\n", getpid() );
+   printf ( "Calling fork now\n\n");
+
+   pid = fork();
+   printf ( "fork has been called,  pid returned is: %d\n", pid);
+
+
+   if( pid == 0)
+
+   {
+
+        printf("I am The child, PID is: %d\n", getpid() );
+	 printf("I am The child, PPID is: %d\n", getppid() );
+        printf("The return value from fork is: %d\n", pid);
+        printf ( "The value of tmp is changed: %d \n\n", tmp*2);
+
+   }
+
+   else if ( pid > 0 )
+
+   {
+        printf ( "I am the parent, PID is: %d\n", getpid() );
+        printf ( "I am the parent, PPID is: %d\n", getppid() );
+	printf ( "The return value from fork is: %d\n", pid );
+        printf ( "The value of tmp is: %d\n\n", tmp);
+   }
+
+     else
+
+        printf ( "Fork failed, no child\n" );
+
+   return 0;
+}
+
